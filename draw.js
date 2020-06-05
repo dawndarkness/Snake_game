51626fdbeb593e9d39bccd6c3deaf2ea32afd0e1
dawndarkness <62203696+dawndarkness@users.noreply.github.com>
2020-06-05 23:11:00 +0900

    Update draw.js

--- draw.js
+++ draw.js
@@ -1,3 +1,34 @@
 const canvas = document.querySelector(".canvas");
 const ctx = canvas.getContext("2d");
-console.log(canvas)
+const scale = 10;
+const rows = canvas.height / scale;
+const columns = canvas.width / scale;
+
+var snake;
+
+(function setup() {
+    snake = new Snake();
+    fruit = new Fruit();
+
+    fruit.pickLocation();
+    console.log(fruit)
+    
+    window.setInterval(() => {
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        fruit.draw();
+        snake.update();
+        snake.draw();
+
+        if (snake.eat(fruit)) {
+            fruit.pickLocation();
+        }
+    }, 250);
+
+}()); 
+
+window.addEventListener('keydown', ((evt) => {
+    console.log(evt);
+    const direction = evt.key.replace('Arrow', '');
+    console.log(direction)
+    snake.changeDirection(direction  )
+})) 
